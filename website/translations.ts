1823ce31e5ae0cd267e36b3e40350c4708fc6283
Lystrillian <webeugene@proton.me>
2024-04-04 17:14:04 +0300

    chore(i18n): added ru-RU translations

--- website/translations.ts
+++ website/translations.ts
@@ -59,4 +59,63 @@ export const translations = {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
     },
   },
+  'ru-RU': {
+    hero: {
+      encodingTool: 'Encodething - инструмент для кодирования',
+      functionalities:
+        'Кодирование, декодирование, шифрование и конвертирование для повышения эффективности работы в Интернете.',
+      convertTailwind: 'Конвертер Tailwind →',
+      dataPlay: 'Поиграем с данными?',
+    },
+    description: {
+      dataTypes: 'Различные типы данных',
+      stringType: 'Строки',
+      numberType: 'Числа',
+      dateType: 'Даты',
+      colorsType: 'Цвета',
+      dataVariety: 'Различные типы данных.',
+      selectData:
+        'Вы можете выбрать тип данных из перечисленных выше и выполнить преобразование.',
+      supported: 'Мы поддерживаем более чем',
+      fifty: '50+',
+      encryptionWays: 'способов шифрования данных',
+      transformationWays: 'Множество способов преобразования данных',
+      encryptionMethods:
+        'У нас есть более 50 различных методов, с помощью которых вы можете зашифровать информацию',
+      greetingRU: 'Привет мир',
+      greetingEN: 'Hello World',
+      greetingJP: 'こんにちは世界',
+      support: 'Мы поддерживаем множество языков.',
+      languages: 'К таковым относятся Русский, Английский, Японский и другие.',
+    },
+    benefits: {
+      autoConfiguration: 'Автоматическая настройка (где это возможно)',
+      configurationEase: 'Вам не нужно беспокоиться о конфигурации',
+      standardValues:
+        'Там, где это возможно, мы уже задали ряд стандартных значений, которые легко изменить позже, если вы захотите протестировать шифр или метод шифрования',
+    },
+    types: {
+      string: 'Строка',
+      stringDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+      number: 'Число',
+      numberDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+      color: 'Цвет',
+      colorDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+      date: 'Дата',
+      dateDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+      cipher: 'Шифр',
+      cipherDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+      hash: 'Хеш',
+      hashDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+      tailwind: 'Tailwind',
+      tailwindDesc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit arcu sed dignissim ultrices. Nullam quis mollis tortor. Suspendisse potenti. Cras id purus a purus aliquet pellentesque vitae et purus. Aliquam vestibulum vulputate massa, non iaculis purus sagittis nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Donec rhoncus rutrum ipsum. Praesent vehicula orci nec maximus cursus. Ut eget tristique mi. Praesent mauris dolor, sagittis convallis dolor malesuada, cursus gravida leo. Nulla feugiat leo non tempus sodales.',
+    },
+  },
 } as const;
