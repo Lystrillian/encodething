339bd95df2dfa571ed00eca2d683755d38a9ab1c
Lystrillian <webeugene@proton.me>
2024-04-17 11:50:54 +0300

    styles: created separate file to store all the icons

---
+++ website/components/Common/Icon/index.tsx
@@ -0,0 +1,27 @@
+export function DescriptionIcon() {
+  return (
+    <>
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7 12.8333C10.2217 12.8333 12.8333 10.2217 12.8333 7C12.8333 3.77834 10.2217 1.16666 7 1.16666C3.77834 1.16666 1.16667 3.77834 1.16667 7C1.16667 10.2217 3.77834 12.8333 7 12.8333Z"
+          fill="#333333"
+        />
+        <path d="M7 9.33333C7 8.42211 7 7.91122 7 7Z" fill="black" />
+        <path d="M7 4.66666H7.00583Z" fill="black" />
+        <path
+          d="M7 9.33333C7 8.42211 7 7.91122 7 7M7 4.66666H7.00583"
+          stroke="white"
+          stroke-width="0.875"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+}
