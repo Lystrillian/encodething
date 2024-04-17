1b63cb2a9b3e47cae91e7e6ef13fe15e98cd58b7
Lystrillian <webeugene@proton.me>
2024-04-17 11:59:54 +0300

    feat(icons): added tick and copy icon

--- website/components/Common/Icon/index.tsx
+++ website/components/Common/Icon/index.tsx
@@ -25,3 +25,47 @@ export function DescriptionIcon() {
     </>
   );
 }
+
+export function Copy() {
+  return (
+    <>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.00001 14.1667C4.07954 14.1667 3.33334 13.4205 3.33334 12.5V4.16667C3.33334 3.24619 4.07954 2.5 5.00001 2.5H10.8333C11.4503 2.5 11.9888 2.83517 12.277 3.33333M9.16668 17.5H15C15.9205 17.5 16.6667 16.7538 16.6667 15.8333V7.5C16.6667 6.57952 15.9205 5.83333 15 5.83333H9.16668C8.2462 5.83333 7.50001 6.57952 7.50001 7.5V15.8333C7.50001 16.7538 8.2462 17.5 9.16668 17.5Z"
+          stroke="#EDEDED"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+}
+
+export function Tick() {
+  return (
+    <>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.6667 5L7.50001 14.1667L3.33334 10"
+          stroke="#EDEDED"
+          stroke-width="1.66667"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+}
