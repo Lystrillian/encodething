3b2612f2854dd4e79fca69b34f895c3aa8306136
Lystrillian <webeugene@proton.me>
2024-04-18 20:24:27 +0300

    style: added Link icon

--- website/components/Common/Icon/index.tsx
+++ website/components/Common/Icon/index.tsx
@@ -2,8 +2,8 @@ export function DescriptionIcon() {
   return (
     <>
       <svg
-        width="14"
-        height="14"
+        width="24"
+        height="24"
         viewBox="0 0 14 14"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -39,9 +39,9 @@ export function Copy() {
         <path
           d="M5.00001 14.1667C4.07954 14.1667 3.33334 13.4205 3.33334 12.5V4.16667C3.33334 3.24619 4.07954 2.5 5.00001 2.5H10.8333C11.4503 2.5 11.9888 2.83517 12.277 3.33333M9.16668 17.5H15C15.9205 17.5 16.6667 16.7538 16.6667 15.8333V7.5C16.6667 6.57952 15.9205 5.83333 15 5.83333H9.16668C8.2462 5.83333 7.50001 6.57952 7.50001 7.5V15.8333C7.50001 16.7538 8.2462 17.5 9.16668 17.5Z"
           stroke="#EDEDED"
-          stroke-width="1.25"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.66667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </>
@@ -64,6 +64,28 @@ export function Tick() {
           stroke-width="1.66667"
           stroke-linecap="round"
           stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+}
+
+export function Link() {
+  return (
+    <>
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12 8.66667V12.6667C12 13.0203 11.8595 13.3594 11.6095 13.6095C11.3594 13.8595 11.0203 14 10.6667 14H3.33333C2.97971 14 2.64057 13.8595 2.39052 13.6095C2.14048 13.3594 2 13.0203 2 12.6667V5.33333C2 4.97971 2.14048 4.64057 2.39052 4.39052C2.64057 4.14048 2.97971 4 3.33333 4H7.33333M10 2H14M14 2V6M14 2L6.66667 9.33333"
+          stroke="#888888"
+          strokeWidth="1.33333"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </>
