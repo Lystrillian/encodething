c7af4d6d8550e59c8cae78b208d21c231636c9ea
Lystrillian <webeugene@proton.me>
2024-04-20 01:54:39 +0300

    feat: added fn to normalize line endings

--- website/lib/utils.ts
+++ website/lib/utils.ts
@@ -18,3 +18,17 @@ export function formatDate(input: string | number): string {
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
 }
+
+export function normalizeLineEndings(value: string, lineEnding: 'LF' | 'CR' | 'CRLF'): string {
+  let normalized = value;
+
+  if (lineEnding === 'LF') {
+    normalized = normalized.replace(/\r\n|\r/g, '\n');
+  } else if (lineEnding === 'CRLF') {
+    normalized = normalized.replace(/\r\n|\r|\n/g, '\r\n');
+  } else if (lineEnding === 'CR') {
+    normalized = normalized.replace(/\r\n|\n/g, '\r');
+  }
+
+  return normalized;
+}
