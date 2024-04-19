d99e7d715f361bb0fe9f05a3ead5b01da82c0c77
Lystrillian <webeugene@proton.me>
2024-04-19 21:04:06 +0300

    refactor: description component refactored

--- website/components/Common/Description/index.tsx
+++ website/components/Common/Description/index.tsx
@@ -1,21 +1,43 @@
 import { DescriptionIcon } from '../Icon';
 import styles from './index.module.css';
+import * as React from 'react';
+import { cn } from 'lib/utils';
 
-export default function Description({
-  title,
-  content,
-}: {
+export interface DescriptionProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
   content: string;
-}) {
-  return (
-    <>
-      <div className={styles.c}>
-        <span className={styles.t}>
-          {title} <DescriptionIcon />
-        </span>
-        <p className={styles.x}>{content}</p>
-      </div>
-    </>
-  );
+  icon?: React.ReactNode;
+  iconClassName?: string;
+  titleClassName?: string;
+  contentClassName?: string;
 }
+
+const Description = React.forwardRef<HTMLDivElement, DescriptionProps>(
+  (
+    {
+      title,
+      content,
+      icon = <DescriptionIcon strokeWidth={1} />,
+      iconClassName,
+      titleClassName,
+      contentClassName,
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <div ref={ref} className={cn(styles.c, className)} {...props}>
+        <div className={styles.h}>
+          <h3 className={cn(styles.t, titleClassName)}>{title}</h3>
+          <span className={cn(styles.i, iconClassName)}>{icon}</span>
+        </div>
+        <div className={cn(styles.x, contentClassName)}>{content}</div>
+      </div>
+    );
+  },
+);
+
+Description.displayName = 'Description';
+
+export { Description };
