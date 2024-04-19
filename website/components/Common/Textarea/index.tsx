610eed075d8eea0872b2b4bfcecf0f02fec1f3a7
Lystrillian <webeugene@proton.me>
2024-04-19 21:38:58 +0300

    refactor: textarea component refactored

--- website/components/Common/Textarea/index.tsx
+++ website/components/Common/Textarea/index.tsx
@@ -1,27 +1,77 @@
+import React, { ChangeEvent, CSSProperties, TextareaHTMLAttributes, useEffect, useRef } from 'react';
+import { cn } from 'lib/utils'; 
 import styles from './index.module.css';
 
-export default function Textarea({
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  value?: string;
+  label?: string;
+  onChange?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  grow?: boolean;
+  onResize?: React.ChangeEventHandler<HTMLTextAreaElement> | undefined;
+  maxHeight?: CSSProperties['maxHeight'];
+  align?: 'left' | 'center' | 'right';
+  status?: 'default' | 'error' | 'success';
+  mode?: 'default' | 'compact';
+}
+
+const Textarea: React.FC<TextareaProps> = ({
   value,
   label,
   onChange,
   placeholder,
-}: {
-  value?: any;
-  label?: string;
-  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  placeholder?: string;
-}) {
+  grow = true,
+  onResize,
+  maxHeight,
+  align = 'left',
+  status = 'default',
+  mode = 'default',
+  ...restProps
+}) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const currentScrollHeight = useRef<number>();
+
+  const autosizeTextarea = () => {
+    const element = textareaRef.current;
+  
+    if (grow && element?.offsetParent) {
+      element.style.height = '';
+      element.style.height = `${element.scrollHeight}px`;
+  
+      if (element.scrollHeight !== currentScrollHeight.current && onResize) {
+        onResize({ target: element } as ChangeEvent<HTMLTextAreaElement>);
+        currentScrollHeight.current = element.scrollHeight;
+      }
+    }
+  };
+
+  useEffect(autosizeTextarea, [grow, textareaRef, onResize]);
+
   return (
-    <>
-      <label htmlFor={label} className={styles.h}>
-        {label}
-      </label>
+    <div
+      className={cn(
+        styles.textareaWrapper,
+        styles[`textareaWrapper--align-${align}`],
+        styles[`textareaWrapper--status-${status}`],
+        styles[`textareaWrapper--mode-${mode}`],
+      )}
+    >
+      {label && (
+        <label htmlFor={label} className={styles.textareaLabel}>
+          {label}
+        </label>
+      )}
       <textarea
-        className={styles.j}
+        ref={textareaRef}
+        className={styles.textarea}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
+        style={{ maxHeight }}
+        {...restProps}
       />
-    </>
+    </div>
   );
-}
+};
+
+export default Textarea;
