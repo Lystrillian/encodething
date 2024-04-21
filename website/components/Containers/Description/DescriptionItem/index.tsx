2f21ab12cec2ba138d4779858fdcb5e5f7fc4876
Lystrillian <webeugene@proton.me>
2024-04-21 18:31:38 +0300

    style: moved description style to a separte stylesheet

--- website/components/Containers/Description/DescriptionItem/index.tsx
+++ website/components/Containers/Description/DescriptionItem/index.tsx
@@ -1,6 +1,6 @@
-// components/ui/DescriptionItem.tsx
 import { cn } from '@utils'
 import * as React from 'react'
+import styles from './index.module.css'
 
 interface DescriptionItemProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
@@ -12,7 +12,7 @@ const DescriptionItem = React.forwardRef<HTMLDivElement, DescriptionItemProps>(
       <div
         ref={ref}
         className={cn(
-          'px-4 bg-layout-main rounded-full min-h-[48px] text-center flex justify-center items-center font-semibold leading-[1.3125] text-ds-gray-900 tracking-[-0.05em] shadow-[0_0_0_1px_rgba(255,255,255,0.14)]',
+          styles.c,
           className
         )}
         style={{ fontSize: 'clamp(1rem, 0.714rem + 1.43vw, 2rem)' }}
