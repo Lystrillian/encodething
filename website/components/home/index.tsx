248263c57b011b9c418cabec5cba6d43a0a0e1a8
Lystrillian <webeugene@proton.me>
2024-04-22 03:31:41 +0300

    chore: encapsulated lenis logic to a separate component

--- website/components/home/index.tsx
+++ website/components/home/index.tsx
@@ -1,25 +1,12 @@
-import Lenis from '@studio-freight/lenis';
-import { useEffect } from 'react';
 import { Hero } from './HeroSection';
 import DescriptionGrid from './DescriptionGrid';
 import BenefitsGrid from './BenefitsGrid';
 import TailwindIcon from '@components/icons/tailwind-icon';
+import useLenis from '@components/Lenis/lenis';
 
 export function Home(): JSX.Element {
-  useEffect(() => {
-    const lenis = new Lenis();
+  useLenis();
 
-    lenis.on('scroll', (e) => {
-      console.log(e);
-    });
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  });
   return (
     <main className="font-overused">
       <Hero />
